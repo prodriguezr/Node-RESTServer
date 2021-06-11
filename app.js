5f47be65dc740a8c18b4657a5dcd2587d9fe33d7
Pablo Rodríguez <prodrigu75@gmail.com>
2021-06-11 00:41:01 -0400

    Espress server has been implemented

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+});
